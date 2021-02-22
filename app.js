52d39e7ddbce58b986c15c3b652aae82579279f9
tpo-ebrito <tpo-ebrito@users.noreply.github.com>
2021-02-22 08:54:25 -0500

    updated photos and code for exceeds

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
+// Imports express
+
 const express = require('express')
 const app = express()
 const port = 3000
 
+// sets data.json to projects
+
 const projects = require('./data.json')
 
-const aboutMe = {
-  name: 'Edson Brito'
-}
+/**
+ *  Sets the static route
+ *  Sets the view engine to pug
+ */
 
 app.use('/static', express.static('public'))
 app.set('view engine', 'pug')
 
+// Root page route. Lists all of the projects
 app.get('/', (req, res) => {
   res.render('index', projects)
 })
 
+// About route. Sets 
 app.get('/about', (req, res) => {
-  res.render('about', aboutMe)
+  res.render('about')
 })
 
-app.get('/contactus', (req, res) => {
-  res.render('about', aboutMe)
-})
-
+// project route. Displays each project in the data.json file.
 app.get('/project/:id', (req, res) => {
   const project = projects.projects[global.parseInt(req.params.id - 1)]
-  res.render('project', {project})
+  res.render('project', { project })
 })
+
+// Errors handlers
 
 app.use((req, res, next) => {
   const err = new Error()
